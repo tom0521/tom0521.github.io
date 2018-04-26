aac30d319f37829a4850d39d5e00301f608e59bd
Thomas Sherwood <tom0521@optonline.net>
2018-04-25 22:07:12 -0400

    removed unused code

--- background.js
+++ background.js
@@ -4,8 +4,6 @@ canvas.width = document.body.offsetWidth;
 var ctx = canvas.getContext("2d");
 var hex = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
 
-
-times = [];
 function loop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   update();
